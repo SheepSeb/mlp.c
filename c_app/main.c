6024e1d2993ea390ebb53699ecbb686fbb0104b1
Sebastian Nae <naesebi2000@gmail.com>
2023-07-26 19:45:14 +0300

    Resolved bug in fc layers

--- c_app/main.c
+++ c_app/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 #include "weights.h"
 #include "flatten_img.h"
 
 int main(){
     float output[50][1] = {0};
-    float input[5][2] = {{1,2},{3,4},{5,6},{7,8},{9,10}};
-    float weight[2][7] = {{1,2,3,4,5,6,7},{8,9,10,11,12,13,14}};
 
     int i,j,k;
 
@@ -34,67 +33,64 @@ int main(){
     }
 
 
+
     /** FC 2 LAYER **/
 
+    float output_fc2[50][1] = {0};
 
     for(i = 0; i < 50; i++){
         for(j = 0; j < 1; j++){
             for(k = 0; k < 50; k++){
-                output[i][j] += fc2_weights[i][k] * output[k][j];
+                output_fc2[i][j] += fc2_weights[i][k] * output[k][j];
             }
         }
     }
 
-
     for(i = 0; i < 50; i++){
         for(j = 0; j < 1; j++){
-            output[i][j] += fc2_bias[i][0];
+            output_fc2[i][j] += fc2_bias[i][0];
         }
     }
 
     for(i = 0; i < 50; i++){
         for(j = 0; j < 1; j++){
-            output[i][j] = output[i][j] > 0 ? output[i][j] : 0;
+            output_fc2[i][j] = output_fc2[i][j] > 0 ? output_fc2[i][j] : 0;
         }
     }
 
-            printf("Output:\n");
-    for(i = 0; i < 50; i++){
-        for(j = 0; j < 1; j++){
-            printf("%f\n", output[i][j]);
-        }
-    }
+
+    float output_fc3[10][1] = {0};
 
     /** FC 3 LAYER **/
 
     for(i = 0; i < 10; i++){
         for(j = 0; j < 1; j++){
-            for(k = 0; k < 10; k++){
-                output[i][j] += fc3_weights[i][k] * output[k][j];
+            for(k = 0; k < 50; k++){
+                output_fc3[i][j] += fc3_weights[i][k] * output_fc2[k][j];
             }
         }
     }
 
     for(i = 0; i < 10; i++){
         for(j = 0; j < 1; j++){
-            output[i][j] += fc3_bias[i][0];
+            output_fc3[i][j] += fc3_bias[i][0];
         }
     }
 
 
     // Do this in C output_fc3_log_softmax = output_fc3 - np.max(output_fc3)
-    float max = output[0][0];
+    float max = output_fc3[0][0];
     for(i = 0; i < 10; i++){
         for(j = 0; j < 1; j++){
-            if(output[i][j] > max){
-                max = output[i][j];
+            if(output_fc3[i][j] > max){
+                max = output_fc3[i][j];
             }
         }
     }
 
     for(i = 0; i < 10; i++){
         for(j = 0; j < 1; j++){
-            output[i][j] -= max;
+            output_fc3[i][j] -= max;
         }
     }
 
@@ -102,30 +98,36 @@ int main(){
     float sum = 0;
     for(i = 0; i < 10; i++){
         for(j = 0; j < 1; j++){
-            sum += exp(output[i][j]);
+            sum += exp(output_fc3[i][j]);
         }
     }
 
     float log_sum = log(sum);
     for(i = 0; i < 10; i++){
         for(j = 0; j < 1; j++){
-            output[i][j] -= log_sum;
+            output_fc3[i][j] -= log_sum;
         }
     }
+
+    // printf("Output FC3:\n");
+    // for(i = 0; i < 10; i++){
+    //     for(j = 0; j < 1; j++){
+    //         printf("%f ", output_fc3[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 
     // Do this in C np.argmax(output_fc3_log_softmax)
     int max_index = 0;
     for(i = 0; i < 10; i++){
         for(j = 0; j < 1; j++){
-            if(output[i][j] > output[max_index][j]){
+            if(output_fc3[i][j] > output_fc3[max_index][j]){
                 max_index = i;
             }
         }
     }
-
-
     
-    printf("Max index: %d\n", max_index);
+    printf("[Max index] Digit Predicted: %d\n", max_index);
 
 
 }
